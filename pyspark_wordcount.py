0ab3ea2a813a8e25d40ed359f93b8ee27bc2ceea
sulyn03 <87534116+sulyn03@users.noreply.github.com>
2025-08-11 23:57:47 +0800

    Update pyspark_wordcount.py

--- pyspark_wordcount.py
+++ pyspark_wordcount.py
@@ -1,6 +1,10 @@
+from pyspark.sql.functions import udf, explode, split, col
+from pyspark.sql.types import StringType
+
 word_counts_df = df.withColumn("CleanedTitle", preprocess_udf(col("Title"))) \
                   .withColumn("word", explode(split(col("CleanedTitle"), " "))) \
                   .filter(col("word") != "") \
                   .groupBy("word") \
                   .count() \
+
                   .sort(col("count").desc())
