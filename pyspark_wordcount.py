59ee4cd81047f51f041c5589572b38032b7ee17d
sulyn03 <87534116+sulyn03@users.noreply.github.com>
2025-08-12 00:01:19 +0800

    Update pyspark_wordcount.py

--- pyspark_wordcount.py
+++ pyspark_wordcount.py
@@ -1,6 +1,9 @@
 from pyspark.sql.functions import udf, explode, split, col
 from pyspark.sql.types import StringType
 
+# Read CSV file from S3 with header and schema inference
+df = spark.read.csv("s3a://stacksample/Questions.csv", header=True, inferSchema=True)
+
 word_counts_df = df.withColumn("CleanedTitle", preprocess_udf(col("Title"))) \
                   .withColumn("word", explode(split(col("CleanedTitle"), " "))) \
                   .filter(col("word") != "") \
@@ -8,3 +11,4 @@ word_counts_df = df.withColumn("CleanedTitle", preprocess_udf(col("Title"))) \
                   .count() \
 
                   .sort(col("count").desc())
+
